5494692d4f03c4b73f7330ca290306136e572228
Patrick Doane <pdoane2@gmail.com>
2023-10-01 15:12:03 -0700

    cg: fix Float definition

--- src/core_graphics/cg.zig
+++ src/core_graphics/cg.zig
@@ -2,7 +2,7 @@ const builtin = @import("builtin");
 
 pub const ColorSpaceRef = *opaque {};
 
-pub const Float = if (builtin.target.ptrBitWidth() == 8) f64 else f32;
+pub const Float = if (builtin.target.ptrBitWidth() == 64) f64 else f32;
 
 pub const Point = extern struct {
     x: Float,
