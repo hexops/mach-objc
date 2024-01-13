a34d29b990499dcbfa985e4ab8e69ebd87275946
Stephen Gutekanst <stephen@hexops.com>
2024-01-13 15:42:02 -0700

    generate CoreMidi
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

---
+++ src/core_midi/coremidi.zig
@@ -0,0 +1,8 @@
+const c = @import("../c.zig");
+const cf = @import("../core_foundation/cf.zig");
+const ns = @import("../foundation/ns.zig");
+
+// ------------------------------------------------------------------------------------------------
+// Types
+
+pub fn init() void {}
