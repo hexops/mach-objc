44f24220f7e2ba8afbe20b8982f341d937fc6e89
Stephen Gutekanst <stephen@hexops.com>
2024-01-13 15:41:52 -0700

    add CoreMidi support
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- generate.sh
+++ generate.sh
@@ -16,4 +16,13 @@ zig run mach-objc-generator/src/main.zig -- --framework AVFAudio >> src/avf_audi
 zig fmt src/avf_audio/avaudio.zig
 rm headers.json headers.m
 
+echo "
+#include <CoreMIDI/MidiServices.h>
+" > headers.m
+clang headers.m -Xclang -ast-dump=json -fsyntax-only > headers.json
+cat core_midi_manual.zig > src/core_midi/coremidi.zig
+zig run mach-objc-generator/src/main.zig -- --framework CoreMIDI >> src/core_midi/coremidi.zig
+zig fmt src/core_midi/coremidi.zig
+rm headers.json headers.m
+
 rm -rf mach-objc-generator
