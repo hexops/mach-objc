bfe6d002fd5a35c0d7d470cd60a58abad14fc1bf
Stephen Gutekanst <stephen@hexops.com>
2024-01-13 15:47:10 -0700

    update to Zig 2024.1.0-mach
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- build.zig
+++ build.zig
@@ -5,38 +5,22 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
 
     _ = b.addModule("mach-objc", .{
-        .source_file = .{ .path = "src/main.zig" },
+        .root_source_file = .{ .path = "src/main.zig" },
     });
 
-    const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&testStep(b, optimize, target).step);
-}
-
-pub fn testStep(b: *std.Build, optimize: std.builtin.OptimizeMode, target: std.zig.CrossTarget) *std.build.RunStep {
     const main_tests = b.addTest(.{
         .name = "mach-objc-tests",
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
     });
-    link(main_tests);
-
-    // Transitive dependencies, explicit linkage of these works around
-    // ziglang/zig#17130
-    main_tests.linkFramework("CoreFoundation");
-
-    // Direct dependencies
+    addPaths(main_tests);
     main_tests.linkFramework("Foundation");
-    b.installArtifact(main_tests);
-    return b.addRunArtifact(main_tests);
+
+    const test_step = b.step("test", "Run library tests");
+    test_step.dependOn(&b.addRunArtifact(main_tests).step);
 }
 
-pub fn link(step: *std.build.CompileStep) void {
-    if (step.target.toTarget().isDarwin()) {
-        @import("xcode_frameworks").addPaths(step);
-
-        // Transitive dependencies, explicit linkage of these works around
-        // ziglang/zig#17130
-        step.linkSystemLibrary("objc");
-    }
+pub fn addPaths(step: *std.Build.Step.Compile) void {
+    @import("xcode_frameworks").addPaths(step);
 }
