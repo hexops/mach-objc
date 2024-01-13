7f8cf296ada11fca1827a04fc113127644475ff2
Stephen Gutekanst <stephen@hexops.com>
2024-01-13 15:41:52 -0700

    add CoreMidi support
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

---
+++ core_midi_manual.zig
@@ -0,0 +1,6 @@
+const c = @import("../c.zig");
+const cf = @import("../core_foundation/cf.zig");
+const ns = @import("../foundation/ns.zig");
+
+// ------------------------------------------------------------------------------------------------
+// Types
