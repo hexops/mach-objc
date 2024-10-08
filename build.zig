1b2196531754f4073977288570f13419ddd004a1
Stephen Gutekanst <stephen@hexops.com>
2024-10-08 09:22:18 -0700

    update to latest xcode-frameworks API
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- build.zig
+++ build.zig
@@ -4,25 +4,30 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
 
-    const objc_module = b.addModule("mach-objc", .{
+    const module = b.addModule("mach-objc", .{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
-    objc_module.linkSystemLibrary("objc", .{});
+    module.linkSystemLibrary("objc", .{});
 
     // TODO: Maybe split this out into a separate module so we only link these in when needed.
     if (target.result.os.tag == .macos) {
-        objc_module.linkFramework("AppKit", .{});
+        module.linkFramework("AppKit", .{});
         if (target.result.cpu.arch == .x86_64) {
-            objc_module.addAssemblyFile(b.path("MACHAppDelegate_x86_64_apple_macos12.s"));
+            module.addAssemblyFile(b.path("MACHAppDelegate_x86_64_apple_macos12.s"));
         } else {
-            objc_module.addAssemblyFile(b.path("MACHAppDelegate_arm64_apple_macos12.s"));
+            module.addAssemblyFile(b.path("MACHAppDelegate_arm64_apple_macos12.s"));
         }
     } else {
-        objc_module.linkFramework("UIKit", .{});
+        module.linkFramework("UIKit", .{});
         // TODO: Add iOS asm files once they are generated.
     }
+
+    const xcode_frameworks = b.dependency("xcode_frameworks", .{});
+    module.addSystemFrameworkPath(xcode_frameworks.path("Frameworks"));
+    module.addSystemIncludePath(xcode_frameworks.path("include"));
+    module.addLibraryPath(xcode_frameworks.path("lib"));
 
     const generator_exe = b.addExecutable(.{
         .name = "generator",
@@ -31,8 +36,4 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
     b.installArtifact(generator_exe);
-}
-
-pub fn addPaths(mod: *std.Build.Module) void {
-    @import("xcode_frameworks").addPaths(mod);
 }
